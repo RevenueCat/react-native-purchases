b417c1694cdf9664c8ca9d48708709a50737b876
Jacob Eiting <jacob.eiting@gmail.com>
2018-06-25 10:48:18 -0700

    Add entitlements to purchaser info dict.

--- ios/RCPurchaserInfo+RNPurchases.m
+++ ios/RCPurchaserInfo+RNPurchases.m
@@ -13,17 +13,26 @@
 - (NSDictionary *)dictionary
 {
     NSISO8601DateFormatter *formatter = [NSISO8601DateFormatter new];
+
     NSMutableDictionary *allExpirations = [NSMutableDictionary new];
     for (NSString *productIdentifier in self.allPurchasedProductIdentifiers) {
         NSDate *date = [self expirationDateForProductIdentifier:productIdentifier];
         allExpirations[productIdentifier] =  date ? [formatter stringFromDate:date] : [NSNull null];
     }
 
+    NSMutableDictionary *expirationsForActiveEntitlements = [NSMutableDictionary new];
+    for (NSString *entId in self.activeEntitlements) {
+        NSDate *date = [self expirationDateForEntitlement:entId];
+        expirationsForActiveEntitlements[entId] = date ? [formatter stringFromDate:date] : [NSNull null];;
+    }
+
     return @{
+             @"activeEntitlements": self.activeEntitlements.allObjects,
              @"activeSubscriptions": self.activeSubscriptions.allObjects,
              @"allPurchasedProductIdentifiers": self.allPurchasedProductIdentifiers.allObjects,
              @"latestExpirationDate": self.latestExpirationDate ?: [NSNull null],
-             @"allExpirationDates": allExpirations
+             @"allExpirationDates": allExpirations,
+             @"expirationsForActiveEntitlements": expirationsForActiveEntitlements
              };
 }
 
