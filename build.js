1dc1f6b4836b926bd2ae8f23317c72df6609cce8
Cesar de la Vega <cesarvegaro@gmail.com>
2020-03-17 20:37:30 -0700

    changes common files to use temporary branch

--- build.js
+++ build.js
@@ -9,7 +9,7 @@ if (os.type() === "Linux") {
   );
   downloadProcess.stdout.pipe(process.stdout);
   const downloadProcessCommon = exec(
-    "./scripts/download-purchases-common.sh 1.0.5"
+    "./scripts/download-purchases-common.sh dev_subscription_offers"
   );
   downloadProcessCommon.stdout.pipe(process.stdout);
 } else if (os.type() === "Windows_NT") {
