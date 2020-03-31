7b15bd306e7a1a98aed1f011470f9726c9c1cca6
Cesar de la Vega <cesarvegaro@gmail.com>
2020-03-31 15:01:44 -0700

    updates android common files

--- android/src/main/java/com/revenuecat/purchases/common/mappers.kt
+++ android/src/main/java/com/revenuecat/purchases/common/mappers.kt
@@ -10,7 +10,6 @@ import com.revenuecat.purchases.PurchaserInfo
 import com.revenuecat.purchases.util.Iso8601Utils
 import java.text.NumberFormat
 import java.util.Currency
-import org.json.JSONObject
 
 fun EntitlementInfo.map(): Map<String, Any?> =
     mapOf(
