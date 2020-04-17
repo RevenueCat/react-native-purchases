9c24ce993c18644ebc88f608b906040b856fef5d
Muhammad-Sharif Moustafa <sharif@revenuecat.com>
2020-04-17 19:25:16 -0400

    Update invalidatePurchaserInfoCache docs
    Invalidating the cache is simultaneously an advanced technique and attractive to beginners who might think they need to invalidate the cache on every app launch, so I added a link to our docs that explains how to work with the cache in normal circumstances. RevenueCat/purchases-ios#223

--- src/index.ts
+++ src/index.ts
@@ -1011,6 +1011,11 @@ export default class Purchases {
 
   /**
    * Invalidates the cache for purchaser information.
+   *
+   * Most apps will not need to use this method; invalidating the cache can leave your app in an invalid state.
+   * Refer to https://docs.revenuecat.com/docs/purchaserinfo#section-get-user-information for more information on
+   * using the cache properly.
+   *
    * This is useful for cases where purchaser information might have been updated outside of the app, like if a
    * promotional subscription is granted through the RevenueCat dashboard.
    */
