cc71b22dfd69cfc480ed3ae4ec70b8c1bdf903d7
Cesar de la Vega <cesarvegaro@gmail.com>
2020-05-28 15:39:14 -0700

    Changes dependency to use maven library

--- android/src/main/java/com/revenuecat/purchases/common/OnResultList.java
+++
@@ -1,9 +0,0 @@
-package com.revenuecat.purchases.common;
-
-import java.util.List;
-import java.util.Map;
-
-public interface OnResultList {
-    void onReceived(List<Map<String, ?>> map);
-    void onError(ErrorContainer errorContainer);
-}
