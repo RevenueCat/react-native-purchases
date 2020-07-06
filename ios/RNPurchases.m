494343f011616e8db0a3cb55137542910beef119
Cesar de la Vega <cesarvegaro@gmail.com>
2020-07-06 14:38:23 -0700

    prepares for next version (#151)

--- ios/RNPurchases.m
+++ ios/RNPurchases.m
@@ -277,7 +277,7 @@ RCT_EXPORT_METHOD(setProxyURLString:(nullable NSString *)proxyURLString)
 }
 
 - (NSString *)platformFlavorVersion {
-    return @"3.3.1";
+    return @"3.4.0-SNAPSHOT";
 }
 
 @end
