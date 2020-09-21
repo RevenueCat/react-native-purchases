81124f8cb4f2bf3bfd4dcbe3e2ddb22d395542fb
Cesar de la Vega <cesarvegaro@gmail.com>
2020-09-21 11:59:04 -0700

    Updates to match bootstrapped plugin (#173)
    * updates to latest version of react native
    
    * adds orb
    
    * aways embed swift standard library
    
    * Revert "aways embed swift standard library"
    
    This reverts commit 4d0ae1af51fcf1092a13399fa1df606828e8f79c.
    
    * use workspace
    
    * workspace
    
    * more fixes
    
    * comments out restoring cache
    
    * more cleanup
    
    * adds downloadDependencies
    
    * removes snapshot repositories
    
    * cache pods
    
    * changes location of yarn caches
    
    * importsNotUsedAsValues doesnt work
    
    * makes npm more lean
    
    * adds podfile lock to gitignore
    
    * revert change
    
    * version updates

--- example/android/app/src/main/java/com/revenuecat/purchases_sample/MainApplication.java
+++ example/android/app/src/main/java/com/revenuecat/purchases_sample/MainApplication.java
@@ -1,16 +1,17 @@
 package com.revenuecat.purchases_sample;
 
 import android.app.Application;
+import android.content.Context;
 
+import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
+import com.facebook.react.ReactInstanceManager;
 import com.revenuecat.purchases.react.RNPurchasesPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
-import java.util.Arrays;
+import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
@@ -23,11 +24,13 @@ public class MainApplication extends Application implements ReactApplication {
 
     @Override
     protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new RNGestureHandlerPackage(),
-            new RNPurchasesPackage()
-      );
+      @SuppressWarnings("UnnecessaryLocalVariable")
+      List<ReactPackage> packages = new PackageList(this).getPackages();
+      // Packages that cannot be autolinked yet can be added manually here, for example:
+      // packages.add(new MyReactNativePackage());
+      packages.add(new RNPurchasesPackage());
+
+      return packages;
     }
 
     @Override
@@ -45,5 +48,34 @@ public class MainApplication extends Application implements ReactApplication {
   public void onCreate() {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
+    initializeFlipper(this, getReactNativeHost().getReactInstanceManager()); // Remove this line if you don't want Flipper enabled
+  }
+
+  /**
+   * Loads Flipper in React Native templates.
+   *
+   * @param context
+   */
+  private static void initializeFlipper(Context context, ReactInstanceManager reactInstanceManager) {
+    if (BuildConfig.DEBUG) {
+      try {
+        /*
+         We use reflection here to pick up the class that initializes Flipper,
+        since Flipper library is not available in release mode
+        */
+        Class<?> aClass = Class.forName("com.revenuecat.purchases_sample.ReactNativeFlipper");
+        aClass
+            .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
+            .invoke(null, context, reactInstanceManager);
+      } catch (ClassNotFoundException e) {
+        e.printStackTrace();
+      } catch (NoSuchMethodException e) {
+        e.printStackTrace();
+      } catch (IllegalAccessException e) {
+        e.printStackTrace();
+      } catch (InvocationTargetException e) {
+        e.printStackTrace();
+      }
+    }
   }
 }
---
+++ example/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  ReactNativeSample
+//
+
+import Foundation
