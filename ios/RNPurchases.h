8ef3cd9852a03fcb559dffb2dd109f68b3a0c09e
Simon Farshid <Yonom@users.noreply.github.com>
2021-12-18 07:10:58 +0100

    Remove old style import

--- ios/RNPurchases.h
+++ ios/RNPurchases.h
@@ -3,11 +3,7 @@
 //  Copyright © 2019 RevenueCat. All rights reserved.
 //
 
-#if __has_include("RCTEventEmitter.h")
-#import "RCTEventEmitter.h"
-#else
 #import <React/RCTEventEmitter.h>
-#endif
 
 #import <Purchases/RCPurchases.h>
 #import <PurchasesHybridCommon/PurchasesHybridCommon.h>
