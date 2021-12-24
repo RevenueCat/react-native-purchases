2c66b0cc7a61b47885cded98174fabc8bb5311b6
Kristoffer Brabrand <kristoffer@brabrand.no>
2021-12-24 13:45:26 +0100

    A fix for the duplicate interfaces error versions of expo using expo modules are unable to build because of duplicate interface definitions

--- ios/RNPurchases.h
+++ ios/RNPurchases.h
@@ -3,12 +3,8 @@
 //  Copyright © 2019 RevenueCat. All rights reserved.
 //
 
-#if __has_include("RCTEventEmitter.h")
-#import "RCTEventEmitter.h"
-#else
+#import <React/RCTAppState.h>
 #import <React/RCTEventEmitter.h>
-#endif
-
 #import <Purchases/RCPurchases.h>
 #import <PurchasesHybridCommon/PurchasesHybridCommon.h>
 
