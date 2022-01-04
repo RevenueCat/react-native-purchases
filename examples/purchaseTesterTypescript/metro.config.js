6f32ca6850941dd66e0647b3077d75e6d0f63cc1
Josh Holtz <me@joshholtz.com>
2022-01-04 15:10:41 -0600

    This is really bad but i think this works

--- examples/purchaseTesterTypescript/metro.config.js
+++ examples/purchaseTesterTypescript/metro.config.js
@@ -27,6 +27,41 @@ module.exports = {
       acc[name] = path.join(__dirname, 'node_modules', name);
       return acc;
     }, {}),
+
+    resolveRequest: (context, realModuleName, platform, moduleName) => {
+      let module = realModuleName;
+        if(realModuleName.includes('@babel/runtime') || moduleName.includes('@babel/runtime')) {
+          // module = module.replace('bla', 'blamos');
+          // if (context.projectRoot === '/Users/joshholtz/Developer/react-native-purchases') {
+          // if (!context.originModulePath) {
+          // console.log("we got a babel", realModuleName, moduleName)
+          if (context.originModulePath.includes('/Users/joshholtz/Developer/react-native-purchases/src')) {
+            
+            // console.log("context", context.originModulePath)
+
+            const delay = 100;
+            var start = new Date().getTime();
+            while (new Date().getTime() < start + delay);
+
+            var extension = '.js'
+            if (realModuleName.includes('regenerator')) {
+              extension = '/index.js'
+            }
+
+            let res = {
+              type: 'sourceFile',
+              filePath: context.projectRoot + '/node_modules/' + realModuleName + extension
+            };
+            // console.log(res)
+            return res
+          }
+        }
+        const { resolveRequest: removed, ...restContext } = context;
+        // console.log('restContext', restContext)
+        let res = require("metro-resolver").resolve(restContext, module, platform);
+        // console.log(res)
+        return res
+    },
   },
 
   transformer: {
