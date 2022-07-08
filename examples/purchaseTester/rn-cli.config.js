d3e6f8f2154c347e5dde059c3e9f4d84d3ace460
NachoSoto <NachoSoto@users.noreply.github.com>
2022-07-08 12:19:49 -0700

    Removed outdated `PurchaseTester` (#364)
    See #320 for the replacement.

--- examples/purchaseTester/rn-cli.config.js
+++
@@ -1,6 +0,0 @@
-const exclusionList = require('metro-config/src/defaults/exclusionList');
-module.exports = {
-  resolver: {
-    blacklistRE: exclusionList([/node_modules\/.*\/node_modules\/react-native\/.*/])
-  }
-};
