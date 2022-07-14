67e4d3e848c47dccc6ffbfa80ababd8208bb7a3a
NachoSoto <ignaciosoto90@gmail.com>
2022-07-14 15:39:15 -0700

    Updated `RNPurchasesModule.java`

--- android/src/main/java/com/revenuecat/purchases/react/RNPurchasesModule.java
+++ android/src/main/java/com/revenuecat/purchases/react/RNPurchasesModule.java
@@ -43,7 +43,7 @@ public class RNPurchasesModule extends ReactContextBaseJavaModule implements Upd
 
     private static final String PURCHASER_INFO_UPDATED = "Purchases-PurchaserInfoUpdated";
     public static final String PLATFORM_NAME = "react-native";
-    public static final String PLUGIN_VERSION = "4.4.1";
+    public static final String PLUGIN_VERSION = "4.6.1";
 
     private final ReactApplicationContext reactContext;
 
