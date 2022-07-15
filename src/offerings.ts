1233a1a6f17ec980afdbc0976145fb59195c5d9b
NachoSoto <NachoSoto@users.noreply.github.com>
2022-07-15 06:08:58 -0700

    Added missing `INTRO_ELIGIBILITY_STATUS_NO_INTRO_OFFER_EXISTS` (#375)

--- src/offerings.ts
+++ src/offerings.ts
@@ -59,7 +59,11 @@ export enum INTRO_ELIGIBILITY_STATUS {
     /**
      * The user is eligible for a free trial or intro pricing for this product.
      */
-    INTRO_ELIGIBILITY_STATUS_ELIGIBLE
+    INTRO_ELIGIBILITY_STATUS_ELIGIBLE,
+    /**
+     * There is no free trial or intro pricing for this product.
+     */
+    INTRO_ELIGIBILITY_STATUS_NO_INTRO_OFFER_EXISTS
 }
 
 
