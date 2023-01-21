106982a03a7d5a876e191959a1e2550ce9200324
Cesar de la Vega <cesarvegaro@gmail.com>
2023-01-20 16:39:02 -0800

    adds missing docs for purchasesconfiguration

--- src/purchases.ts
+++ src/purchases.ts
@@ -109,11 +109,35 @@ export interface LogInResult {
  * Holds parameters to initialize the SDK.
  */
 export interface PurchasesConfiguration {
+  /**
+   * RevenueCat API Key. Needs to be a string
+   */
   apiKey: string;
+  /**
+   * A unique id for identifying the user
+   */
   appUserID?: string | null;
+  /**
+   * An optional boolean. Set this to TRUE if you have your own IAP implementation and
+   * want to use only RevenueCat's backend. Default is FALSE. If you are on Android and setting this to ON, you will have
+   * to acknowledge the purchases yourself.
+   */
   observerMode?: boolean;
+  /**
+   * An optional string. iOS-only, will be ignored for Android.
+   * Set this if you would like the RevenueCat SDK to store its preferences in a different NSUserDefaults
+   * suite, otherwise it will use standardUserDefaults. Default is null, which will make the SDK use standardUserDefaults.
+   */
   userDefaultsSuiteName?: string;
+  /**
+   * iOS-only, will be ignored for Android.
+   * Set this to FALSE to disable StoreKit2.
+   * Default is TRUE.
+   */
   usesStoreKit2IfAvailable?: boolean;
+  /**
+   * An optional boolean. Android only. Required to configure the plugin to be used in the Amazon Appstore.
+   */
   useAmazon?: boolean;
 }
 
