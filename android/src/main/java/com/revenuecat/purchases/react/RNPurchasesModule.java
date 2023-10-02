5c4e42066258d78c6d1a4a86e01529ce999d5cd7
RevenueCat CI <dev+ci@revenuecat.com>
2023-10-02 16:25:07 +0000

    Version bump for 6.7.0

--- android/src/main/java/com/revenuecat/purchases/react/RNPurchasesModule.java
+++ android/src/main/java/com/revenuecat/purchases/react/RNPurchasesModule.java
@@ -45,7 +45,7 @@ public class RNPurchasesModule extends ReactContextBaseJavaModule implements Upd
     private static final String CUSTOMER_INFO_UPDATED = "Purchases-CustomerInfoUpdated";
     private static final String LOG_HANDLER_EVENT = "Purchases-LogHandlerEvent";
     public static final String PLATFORM_NAME = "react-native";
-    public static final String PLUGIN_VERSION = "6.7.0-SNAPSHOT";
+    public static final String PLUGIN_VERSION = "6.7.0";
 
     private final ReactApplicationContext reactContext;
 
