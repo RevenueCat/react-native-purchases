8c3a18eb7feb4410d717e067966fb5dc5b313466
Cesar de la Vega <cesarvegaro@gmail.com>
2024-01-10 15:13:05 +0100

    creates static functions

--- react-native-purchases-ui/src/index.tsx
+++ react-native-purchases-ui/src/index.tsx
@@ -1,31 +1,31 @@
-import {NativeModules, Platform, requireNativeComponent, UIManager} from "react-native";
-import {PAYWALL_RESULT} from "@revenuecat/purchases-typescript-internal";
+import { NativeModules, Platform, requireNativeComponent, UIManager } from "react-native";
+import { PAYWALL_RESULT } from "@revenuecat/purchases-typescript-internal";
 
-export {PAYWALL_RESULT} from "@revenuecat/purchases-typescript-internal";
+export { PAYWALL_RESULT } from "@revenuecat/purchases-typescript-internal";
 
 const LINKING_ERROR =
   `The package 'react-native-purchases-view' doesn't seem to be linked. Make sure: \n\n` +
-  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
+  Platform.select({ios: "- You have run 'pod install'\n", default: ''}) +
   '- You rebuilt the app after installing the package\n' +
   '- You are not using Expo Go\n';
 
 const RNPaywalls = NativeModules.RNPaywalls;
+export default class RevenueCatUI {
 
-export function presentPaywall(): Promise<PAYWALL_RESULT> {
-  // TODO: check iOS/Android version
-  return RNPaywalls.presentPaywall();
+  public static presentPaywall(): Promise<PAYWALL_RESULT> {
+    // TODO: check iOS/Android version
+    return RNPaywalls.presentPaywall();
+  }
+
+  public static presentPaywallIfNeeded(requiredEntitlementIdentifier: string): Promise<PAYWALL_RESULT> {
+    // TODO: check iOS/Android version
+    return RNPaywalls.presentPaywallIfNeeded(requiredEntitlementIdentifier);
+  }
+
+  public static Paywall =
+    UIManager.getViewManagerConfig('Paywall') != null
+      ? requireNativeComponent('Paywall')
+      : () => {
+        throw new Error(LINKING_ERROR);
+      };
 }
-
-export function presentPaywallIfNeeded(requiredEntitlementIdentifier: string): Promise<PAYWALL_RESULT> {
-  // TODO: check iOS/Android version
-  return RNPaywalls.presentPaywallIfNeeded(requiredEntitlementIdentifier);
-}
-
-const ComponentName = 'Paywall';
-
-export const Paywall =
-  UIManager.getViewManagerConfig(ComponentName) != null
-    ? requireNativeComponent(ComponentName)
-    : () => {
-      throw new Error(LINKING_ERROR);
-    };
