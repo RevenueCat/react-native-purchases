9e41e952ed38af2fe9a6c791ab86221bc480749d
Cesar de la Vega <cesarvegaro@gmail.com>
2024-01-23 14:52:37 +0100

    PaywallViewManager: added controller to UIVC hierarchy

--- react-native-purchases-ui/ios/PaywallViewManager.m
+++ react-native-purchases-ui/ios/PaywallViewManager.m
@@ -7,7 +7,52 @@
 //
 
 #import "PaywallViewManager.h"
+#import "UIView+Extensions.h"
 @import PurchasesHybridCommon;
+
+@interface PaywallView : UIView
+
+@property (strong, nonatomic) UIViewController *paywallViewController;
+@property  BOOL addedToHierarchy;
+
+@end
+
+@implementation PaywallView
+
+- (instancetype)initWithPaywallViewController:(UIViewController *)paywallViewController
+                                    innerView:(UIView *)innerView {
+    if ((self = [super initWithFrame:innerView.bounds])) {
+        _paywallViewController = paywallViewController;
+        [self addSubview:innerView];
+        _addedToHierarchy = NO;
+    }
+    return self;
+}
+
+- (void)layoutSubviews {
+    [super layoutSubviews];
+
+    if (!self.addedToHierarchy) {
+        UIViewController *parentController = self.parentViewController;
+        if (parentController) {
+            self.paywallViewController.view.translatesAutoresizingMaskIntoConstraints = NO;
+            [parentController addChildViewController:self.paywallViewController];
+            [self addSubview:self.paywallViewController.view];
+            [self.paywallViewController didMoveToParentViewController:parentController];
+
+            [NSLayoutConstraint activateConstraints:@[
+                [self.paywallViewController.view.topAnchor constraintEqualToAnchor:self.topAnchor],
+                [self.paywallViewController.view.bottomAnchor constraintEqualToAnchor:self.bottomAnchor],
+                [self.paywallViewController.view.leftAnchor constraintEqualToAnchor:self.leftAnchor],
+                [self.paywallViewController.view.rightAnchor constraintEqualToAnchor:self.rightAnchor]
+            ]];
+
+            self.addedToHierarchy = YES;
+        }
+    }
+}
+
+@end
 
 @implementation PaywallViewManager
 
@@ -17,7 +62,12 @@ RCT_EXPORT_MODULE(Paywall)
 {
     if (@available(iOS 15.0, *)) {
         PaywallProxy *proxy = [[PaywallProxy alloc] init];
-        return [proxy createPaywallView].view;
+        UIViewController *viewController = [proxy createPaywallView];
+        UIView *innerView = viewController.view;
+
+        PaywallView *paywallView = [[PaywallView alloc] initWithPaywallViewController:viewController
+                                                                            innerView:innerView];
+        return paywallView;
     } else {
         NSLog(@"Error: attempted to present paywalls on unsupported iOS version.");
         return nil;
