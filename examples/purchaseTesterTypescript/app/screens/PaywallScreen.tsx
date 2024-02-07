5599e91ae0a8382df998db91252a927367b80c4c
Cesar de la Vega <cesarvegaro@gmail.com>
2024-02-07 15:21:16 +0100

    add listeners to PaywallScreen

--- examples/purchaseTesterTypescript/app/screens/PaywallScreen.tsx
+++ examples/purchaseTesterTypescript/app/screens/PaywallScreen.tsx
@@ -4,10 +4,44 @@ import RevenueCatUI from 'react-native-purchases-ui';
 import { StyleSheet, View } from 'react-native';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import RootStackParamList from '../RootStackParamList';
+import {
+  CustomerInfo,
+  PurchasesError,
+  PurchasesPackage,
+  PurchasesStoreTransaction
+} from "@revenuecat/purchases-typescript-internal";
 
 type Props = NativeStackScreenProps<RootStackParamList, 'Paywall'>;
 
 const PaywallScreen: React.FC<Props> = ({ route }: Props) => {
+  // Example handlers for the events
+  const onPurchaseStarted = (aPackage: PurchasesPackage) => {
+    console.log('Purchase started for package:', aPackage);
+  };
+
+  const onPurchaseCompleted = (customerInfo: CustomerInfo, storeTransaction: PurchasesStoreTransaction) => {
+    console.log('Purchase completed:', customerInfo, storeTransaction);
+  };
+
+  const onPurchaseError = (error: PurchasesError) => {
+    console.error('Purchase error:', error);
+  };
+
+  const onPurchaseCancelled = () => {
+    console.log('Purchase was cancelled');
+  };
+
+  const onRestoreStarted = () => {
+    console.log('Restore started');
+  };
+
+  const onRestoreCompleted = (customerInfo: CustomerInfo) => {
+    console.log('Restore completed:', customerInfo);
+  };
+
+  const onRestoreError = (error: PurchasesError) => {
+    console.error('Restore error:', error);
+  };
   const styles = StyleSheet.create({
     flex1: {
       flex: 1,
@@ -18,7 +52,15 @@ const PaywallScreen: React.FC<Props> = ({ route }: Props) => {
       <RevenueCatUI.Paywall
         options={{
           offering: route.params.offering,
-        }}/>
+        }}
+        onPurchaseStarted={onPurchaseStarted}
+        onPurchaseCompleted={onPurchaseCompleted}
+        onPurchaseError={onPurchaseError}
+        onPurchaseCancelled={onPurchaseCancelled}
+        onRestoreStarted={onRestoreStarted}
+        onRestoreCompleted={onRestoreCompleted}
+        onRestoreError={onRestoreError}
+      />
     </View>
   );
 };
