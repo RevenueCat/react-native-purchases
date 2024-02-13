f237f1ac429a77e39814cd66d6e8e8a28a5b18ee
Cesar de la Vega <cesarvegaro@gmail.com>
2024-02-13 14:54:58 +0100

    format file

--- react-native-purchases-ui/ios/PaywallViewWrapper.m
+++ react-native-purchases-ui/ios/PaywallViewWrapper.m
@@ -15,27 +15,28 @@
 API_AVAILABLE(ios(15.0))
 @interface PaywallViewWrapper ()
 
-@property (strong, nonatomic) RCPaywallViewController *paywallViewController;
+@property(strong, nonatomic)RCPaywallViewController *paywallViewController;
 
-@property (nonatomic) BOOL addedToHierarchy;
+@property(nonatomic) BOOL addedToHierarchy;
 
 @end
 
 @implementation PaywallViewWrapper
 
-- (instancetype)initWithPaywallViewController:(RCPaywallViewController *)paywallViewController API_AVAILABLE(ios(15.0)){
+- (instancetype)initWithPaywallViewController:(RCPaywallViewController *)paywallViewController
+API_AVAILABLE(ios(15.0)) {
     NSParameterAssert(paywallViewController);
-
+    
     if ((self = [super initWithFrame:paywallViewController.view.bounds])) {
         _paywallViewController = paywallViewController;
     }
-
+    
     return self;
 }
 
 - (void)layoutSubviews {
     [super layoutSubviews];
-
+    
     // Need to wait for this view to be in the hierarchy to look for the parent UIVC.
     // This is required to add a SwiftUI `UIHostingController` to the hierarchy in a way that allows
     // UIKit to read properties from the environment, like traits and safe area.
@@ -46,14 +47,14 @@ API_AVAILABLE(ios(15.0))
             [parentController addChildViewController:self.paywallViewController];
             [self addSubview:self.paywallViewController.view];
             [self.paywallViewController didMoveToParentViewController:parentController];
-
+            
             [NSLayoutConstraint activateConstraints:@[
                 [self.paywallViewController.view.topAnchor constraintEqualToAnchor:self.topAnchor],
                 [self.paywallViewController.view.bottomAnchor constraintEqualToAnchor:self.bottomAnchor],
                 [self.paywallViewController.view.leftAnchor constraintEqualToAnchor:self.leftAnchor],
                 [self.paywallViewController.view.rightAnchor constraintEqualToAnchor:self.rightAnchor]
             ]];
-
+            
             self.addedToHierarchy = YES;
         }
     }
@@ -73,16 +74,16 @@ API_AVAILABLE(ios(15.0))
     }
 }
 
-- (void) paywallViewControllerDidStartPurchase:(RCPaywallViewController *)controller API_AVAILABLE(ios(15.0)) {
+- (void)paywallViewControllerDidStartPurchase:(RCPaywallViewController *)controller API_AVAILABLE(ios(15.0)) {
     // TODO We need to send the package being purchased to match Android
 }
 
-- (void)                paywallViewController:(RCPaywallViewController *)controller
+- (void)paywallViewController:(RCPaywallViewController *)controller
 didFinishPurchasingWithCustomerInfoDictionary:(NSDictionary *)customerInfoDictionary
-                        transactionDictionary:(NSDictionary *)transactionDictionary API_AVAILABLE(ios(15.0)) {
+        transactionDictionary:(NSDictionary *)transactionDictionary API_AVAILABLE(ios(15.0)) {
     self.onPurchaseCompleted(@{
-        @"customerInfo": customerInfoDictionary,
-        @"storeTransaction": transactionDictionary,
+        @"customerInfo" : customerInfoDictionary,
+        @"storeTransaction" : transactionDictionary,
     });
 }
 
@@ -90,30 +91,23 @@ didFinishPurchasingWithCustomerInfoDictionary:(NSDictionary *)customerInfoDictio
     self.onPurchaseCancelled(nil);
 }
 
-- (void)          paywallViewController:(RCPaywallViewController *)controller
-   didFailPurchasingWithErrorDictionary:(NSDictionary *)errorDictionary API_AVAILABLE(ios(15.0)) {
-    self.onPurchaseError(@{
-        @"error": errorDictionary
-    });
+- (void)paywallViewController:(RCPaywallViewController *)controller
+didFailPurchasingWithErrorDictionary:(NSDictionary *)errorDictionary API_AVAILABLE(ios(15.0)) {
+    self.onPurchaseError(@{@"error" : errorDictionary});
 }
 
-- (void)               paywallViewController:(RCPaywallViewController *)controller
+- (void)paywallViewController:(RCPaywallViewController *)controller
 didFinishRestoringWithCustomerInfoDictionary:(NSDictionary *)customerInfoDictionary API_AVAILABLE(ios(15.0)) {
-    self.onRestoreCompleted(@{
-        @"customerInfo": customerInfoDictionary
-    });
+    self.onRestoreCompleted(@{@"customerInfo" : customerInfoDictionary});
 }
 
-- (void)      paywallViewController:(RCPaywallViewController *)controller
+- (void)paywallViewController:(RCPaywallViewController *)controller
 didFailRestoringWithErrorDictionary:(NSDictionary *)errorDictionary API_AVAILABLE(ios(15.0)) {
-    self.onRestoreError(@{
-        @"error": errorDictionary
-    });
+    self.onRestoreError(@{@"error" : errorDictionary});
 }
 
-- (void) paywallViewControllerWasDismissed:(RCPaywallViewController *)controller API_AVAILABLE(ios(15.0)) {
+- (void)paywallViewControllerWasDismissed:(RCPaywallViewController *)controller API_AVAILABLE(ios(15.0)) {
     self.onDismiss(nil);
 }
-
 
 @end
