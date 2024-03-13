70e72dd2ea1c758b0cbb748d571beaae543878ad
Josh Holtz <me@joshholtz.com>
2024-03-13 07:21:01 -0500

    Remove keys

--- examples/purchaseTesterTypescript/app/APIKeys.tsx
+++ examples/purchaseTesterTypescript/app/APIKeys.tsx
@@ -1,8 +1,9 @@
+
 // REPLACE THESE WITH YOUR OWN
 const APIKeys = {
-  apple: 'appl_KFpRDwauDyfIGIETKyVymslMcjX',
-  google: 'goog_cRtaFpfwfcQYPXkIEqFUkVszUfN',
-  amazon: '',
+  apple: "",
+  google: "",
+  amazon: ""
 };
 
 export default APIKeys;
