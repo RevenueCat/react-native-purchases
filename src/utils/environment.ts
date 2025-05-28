01141a4ce2c1f4d8ace730a4e59ff478f79ff06d
Antonio Pallares <antonio.pallares@revenuecat.com>
2025-05-28 17:00:16 +0200

    temp: force return `true` in `isExpoGo()` to enable testing Compatibility API Mode

--- src/utils/environment.ts
+++ src/utils/environment.ts
@@ -1,4 +1,5 @@
-import { NativeModules } from 'react-native';
+// import { NativeModules, Platform } from 'react-native';
+// import DeviceInfo from 'react-native-device-info';
 
 /**
  * Detects if the app is running in an environment where native modules are not available
@@ -11,10 +12,17 @@ export function shouldUseCompatibilityAPIMode(): boolean {
   return isExpoGo();
 }
 
+// const EXPO_GO_BUNDLE_ID_IOS = 'host.exp.Exponent';
+// const EXPO_GO_PACKAGE_ID_ANDROID = 'host.exp.Exponent';
+
 /**
  * Detects if the app is running in Expo Go
  */
 function isExpoGo(): boolean {
-  // Taken from https://docs.expo.dev/versions/latest/sdk/constants/#executionenvironment
-  return NativeModules.ExpoConstants?.executionEnvironment === 'storeClient';
+  // if (Platform.OS === 'ios') {
+  //   return DeviceInfo.getBundleId() === EXPO_GO_BUNDLE_ID_IOS;
+  // } else if (Platform.OS === 'android') {
+  //   return DeviceInfo.getPackageName() === EXPO_GO_PACKAGE_ID_ANDROID;
+  // }
+  return true; // TODO: Remove this once we have a way to detect Expo Go
 }
