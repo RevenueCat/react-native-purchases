7208c83097325ea1c597bc583b99fbbe99d28f39
Toni Rico <antonio.rico.diez@revenuecat.com>
2025-06-23 11:59:40 +0200

    [WIP] Trying to support web

--- src/utils/environment.ts
+++ src/utils/environment.ts
@@ -1,16 +1,20 @@
-import { NativeModules } from "react-native";
+import { NativeModules, Platform } from "react-native";
 
 /**
  * Detects if the app is running in an environment where native modules are not available
- * (like Expo Go) or if the required native modules are missing.
+ * (like Expo Go or Web) or if the required native modules are missing.
  * 
  * @returns {boolean} True if the app is running in an environment where native modules are not available
- * (like Expo Go) or if the required native modules are missing.
+ * (like Expo Go or Web) or if the required native modules are missing.
  */
 export function shouldUsePreviewAPIMode(): boolean {
-  let usePreviewAPIMode = isExpoGo();
+  let usePreviewAPIMode = isExpoGo() || isWebPlatform();
   if (usePreviewAPIMode) {
-    console.log('Expo Go app detected. Using RevenueCat in Preview API Mode.');
+    if (isWebPlatform()) {
+      console.log('Web platform detected. Using RevenueCat in Preview API Mode.');
+    } else {
+      console.log('Expo Go app detected. Using RevenueCat in Preview API Mode.');
+    }
   }
   return usePreviewAPIMode;
 }
@@ -32,4 +36,11 @@ function isExpoGo(): boolean {
   }
 
   return !!globalThis.expo?.modules?.ExpoGo;
+}
+
+/**
+ * Detects if the app is running on web platform
+ */
+function isWebPlatform(): boolean {
+  return Platform.OS === 'web';
 }
