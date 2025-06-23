42a1d4c77f076a072695aee348ee73fd782d14d8
Toni Rico <antonio.rico.diez@revenuecat.com>
2025-06-23 13:54:33 +0200

    Fix warning

--- examples/purchaseTesterTypescript/index.js
+++ examples/purchaseTesterTypescript/index.js
@@ -4,7 +4,7 @@
 
 import {AppRegistry, Platform} from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import {name as appName} from './App.json';
 
 AppRegistry.registerComponent(appName, () => App);
 
