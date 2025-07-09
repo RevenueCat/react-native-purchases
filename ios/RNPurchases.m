2eb9438733fdb20809d18d8ad373e6da94045eb7
Antonio Pallares <ajpallares@users.noreply.github.com>
2025-07-09 16:15:21 +0200

    Fix crash on refresh app on the iOS simulator (#1320)

--- ios/RNPurchases.m
+++ ios/RNPurchases.m
@@ -42,6 +42,14 @@ NSString *RNPurchasesLogHandlerEvent = @"Purchases-LogHandlerEvent";
              RNPurchasesLogHandlerEvent];
 }
 
+- (void)sendEventWithName:(NSString *)name body:(id)body {
+    /// This prevents a potential crash that can occur when the JS modules are not set up yet.
+    /// See https://github.com/RevenueCat/react-native-purchases/issues/1305
+    if (self.callableJSModules != nil) {
+        [super sendEventWithName:name body:body];
+    }
+}
+
 RCT_EXPORT_MODULE();
 
 RCT_EXPORT_METHOD(setupPurchases:(NSString *)apiKey
