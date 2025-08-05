f7f3a7e98a696532d6061fa3a89883ac684967d5
Facundo Menzella <facumenzella@gmail.com>
2025-08-05 18:46:14 +0200

    revert api key

--- examples/purchaseTesterTypescript/app/APIKeys.tsx
+++ examples/purchaseTesterTypescript/app/APIKeys.tsx
@@ -1,8 +1,8 @@
 
 // REPLACE THESE WITH YOUR OWN
 const APIKeys = {
-  apple: "appl_SYkbbYptqjDadVkQeBUtXxVxCcw",
-  google: "goog_TzjuPFWcFelhzMSOGvKoZWvrdcW",
+  apple: "",
+  google: "",
   amazon: ""
 };
 
