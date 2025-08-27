93804afbaaf171ce1edceb886b9615f5ec8dff91
Facundo Menzella <facumenzella@gmail.com>
2025-08-27 12:52:20 +0200

    revert eventname

--- react-native-purchases-ui/android/src/main/java/com/revenuecat/purchases/react/ui/customercenter/events/CustomerCenterEventName.kt
+++ react-native-purchases-ui/android/src/main/java/com/revenuecat/purchases/react/ui/customercenter/events/CustomerCenterEventName.kt
@@ -2,5 +2,4 @@ package com.revenuecat.purchases.react.ui.customercenter.events
 
 internal enum class CustomerCenterEventName(val eventName: String) {
     ON_DISMISS("onDismiss"),
-    ON_CUSTOM_ACTION_SELECTED("onCustomActionSelected");
 }
