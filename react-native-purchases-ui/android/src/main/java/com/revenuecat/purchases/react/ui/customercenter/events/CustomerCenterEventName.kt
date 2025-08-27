993ddf0d7bf0f8474428b0f71799bb9d9bef6bf3
Facundo Menzella <facumenzella@gmail.com>
2025-08-27 12:57:41 +0200

    remove test

--- react-native-purchases-ui/android/src/main/java/com/revenuecat/purchases/react/ui/customercenter/events/CustomerCenterEventName.kt
+++ react-native-purchases-ui/android/src/main/java/com/revenuecat/purchases/react/ui/customercenter/events/CustomerCenterEventName.kt
@@ -1,5 +1,5 @@
 package com.revenuecat.purchases.react.ui.customercenter.events
 
 internal enum class CustomerCenterEventName(val eventName: String) {
-    ON_DISMISS("onDismiss"),
+    ON_DISMISS("onDismiss");
 }
