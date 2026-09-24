19eadcff4fe9fd1d3e358a016378d132ed894c40
Álvaro Brey <alvaro.brey@revenuecat.com>
2026-09-24 11:27:57 +0200

    build: drop the purchaseTester import of the removed NewAppScreen

---
+++ examples/purchaseTesterTypescript/app/colors.ts
@@ -0,0 +1,8 @@
+export const Colors = {
+  white: '#FFF',
+  lighter: '#F3F3F3',
+  light: '#DAE1E7',
+  dark: '#444',
+  darker: '#222',
+  black: '#000',
+};
--- examples/purchaseTesterTypescript/app/screens/WinBackTestingScreen.tsx
+++ examples/purchaseTesterTypescript/app/screens/WinBackTestingScreen.tsx
@@ -7,7 +7,6 @@ import {
   TouchableOpacity,
   View,
 } from 'react-native';
-import {Colors} from 'react-native/Libraries/NewAppScreen';
 import Purchases, {
   PurchasesPackage,
   PurchasesStoreProduct,
